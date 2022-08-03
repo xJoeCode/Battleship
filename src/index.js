acb8f2ba2d40946b1a49322248a9d170cfd3e906
xJoeCode <xjosephtbr@gmail.com>
2022-08-03 16:31:07 +0800

    added a checkhits function that compares with 2 arrays to check if clicked values matches ship position values

--- src/index.js
+++ src/index.js
@@ -16,16 +16,19 @@ const ship = (length, orient) =>{
             position.push(initialPos + (i*10))
         }
        }
-
     })()
-    
     return {getLength, position}
 }
 
 
 const albama = ship(3, "landscape")
 const chicken = ship(4, "portrait")
-console.log(chicken.position)
+let allShipPos = []
+allShipPos.push(...chicken.position)
+allShipPos.push(...albama.position)
+console.log(allShipPos)
+
+let hits = []
 
 const generateboard = () => {
     const gameTile = document.querySelector("#gameTile")
@@ -33,14 +36,25 @@ const generateboard = () => {
     for(let i =0; i <100; i++){
        const tile = gameTile.cloneNode()
        tile.setAttribute('data-key', i+1)
+       tile.onclick = function(e){
+           tile.classList.add("hit")
+           const hitNum = parseInt(e.target.getAttribute('data-key'))
+           console.log(hitNum)
+           hits.push(hitNum)
+           checkHits()
+       }
        gameContainer.appendChild(tile)
     }
     gameTile.remove()
 }
 
-const placeShips = () =>{
-    const marker = math.random() *100
+const checkHits = () =>{
+    console.log(hits)
+    let hit = allShipPos.filter(position =>  hits.includes(position))
+    console.log(hit)
 }
+
+
 
 
 generateboard()
