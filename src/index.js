8e8c660355bdd07431106b9c5be178e8cdb78bea
xJoeCode <xjosephtbr@gmail.com>
2022-08-03 15:12:25 +0800

    added ship orientation and more css styling for layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import "./style.css";
 
-const ship = (length) =>{
+const ship = (length, orient) =>{
     const getLength = () => length
     let position = []
+    let hit = []
+
+    const shipPos = (() => {
+       const initialPos = Math.floor(Math.random()*100)
+       if(orient === "landscape"){
+            for (let i = 0; i < length; i++){
+                position.push(initialPos + i)
+            }
+       } else if (orient === "portrait"){
+        for (let i = 0; i < length; i++){
+            position.push(initialPos + (i*10))
+        }
+       }
+
+    })()
+    
     return {getLength, position}
 }
 
 
-const albama = ship(10)
-console.log(albama.getLength())
+const albama = ship(3, "landscape")
+const chicken = ship(4, "portrait")
+console.log(chicken.position)
 
 const generateboard = () => {
     const gameTile = document.querySelector("#gameTile")
@@ -19,6 +36,10 @@ const generateboard = () => {
        gameContainer.appendChild(tile)
     }
     gameTile.remove()
+}
+
+const placeShips = () =>{
+    const marker = math.random() *100
 }
 
 
